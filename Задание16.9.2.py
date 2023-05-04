dde0efc114af2145ed4f26a336be2a3c76bd3199
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-04 18:51:19 +0500

    Delete Задание16.9.2.py

--- Задание16.9.2.py
+++
@@ -1,9 +0,0 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    def get_area(self):
-        print (self.width * self.height)
-
-res = Rectangle(5, 3)
-res.get_area()
