846c7dae02ab938b80d1349a5d7709afebf16b70
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-04 18:53:46 +0500

    Delete 18.6_KP.py

--- 18.6_KP.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-TOKEN = "5859319186:AAEd0vrcCMbmZ0OVJd6APkxjivOHYa9zHb0 "
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message.handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
