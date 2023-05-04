b5eded59d8a43fd6642b0c9b70e6a5cc910ff71b
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-04 18:51:35 +0500

    Delete Задание16.9.1.py

--- Задание16.9.1.py
+++
@@ -1,9 +0,0 @@
-class Rectangle:
-    def __init__(self, x, y, widyh, heght):
-      self.x = x
-      self.y = y
-      self.width = widyh
-      self.heght = heght
-    def __str__(self):
-      return f"{self.x, self.y, self.width, self.heght}"
-
