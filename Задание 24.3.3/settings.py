7c85b17846094d1f23422e461814c5cef6ca50e2
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-05 14:05:40 +0500

    Delete Задание 24.3.3 directory

--- Задание 24.3.3/settings.py
+++
@@ -1,9 +0,0 @@
-﻿valid_email ="serg.nik.z@yandex.ru"
-valid_password="123"
-pet_photo=r"C:\Users\Сергей\PycharmProjects\Модуль_24\pet_photo.jpg"
-base_url="https://petfriends.skillfactory.ru/"
-
-name = 'Сигма'
-animal_type = 'Собака'
-age = '1'
-name1='Шарик'
