85db85f72e66925952972052c62c52aeba7c4c97
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-05 14:06:03 +0500

    Delete Задание 24.7.2 directory

--- Задание 24.7.2/settinds.py
+++
@@ -1,7 +0,0 @@
-valid_email ="serg.nik.z@yandex.ru"
-valid_password="123"
-base_url="https://petfriends.skillfactory.ru/"
-name = 'Василий'
-animal_type = "кот"
-age = 2
-photo_k =r"C:\Users\Сергей\PycharmProjects\Pet_Friend_Test\photo_k.jpg"
