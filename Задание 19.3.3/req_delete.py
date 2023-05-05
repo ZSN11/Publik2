aec22d0c455b0bb0dd74e17b2a73b397d35dee82
ZSN11 <112653546+ZSN11@users.noreply.github.com>
2023-05-05 14:00:18 +0500

    Delete Задание 19.3.3 directory

--- Задание 19.3.3/req_delete.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# status = 'available'
-
-res=requests.delete(f'https://petstore.swagger.io/v2/pet/12',headers={'accept':'application/json'} )
-
-
-print(res.status_code)
-print(res.text)
